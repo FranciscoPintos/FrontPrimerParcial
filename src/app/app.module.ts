f6176feddb5116bea16f21300f3dd381178a7fa9
franpintos <pintos98javier99@fpuna.edu.py>
2022-08-17 18:20:16 -0400

    Add Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import {HttpClientModule} from "@angular/common/http";
 import {ServicepaisService} from "./service/servicepais.service";
 import { PaisAgregarComponent } from './pais/pais-agregar/pais-agregar.component';
 import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -20,6 +24,10 @@ import {FormsModule} from "@angular/forms";
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [ServicepaisService,],
   bootstrap: [AppComponent]
