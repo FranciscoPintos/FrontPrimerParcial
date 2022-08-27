6a807ab33b7932be1c740e962b9539a203c41f54
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-08-26 20:40:01 -0400

    Solo importamos el shared module

--- src/app/ficha-clinica/ficha-clinica.module.ts
+++ src/app/ficha-clinica/ficha-clinica.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -21,15 +22,7 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCardModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatButtonModule,
+    SharedModule
   ],
   providers: [
     MatDatepickerModule
