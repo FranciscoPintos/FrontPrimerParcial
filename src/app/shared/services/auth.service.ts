b865172ae44bc3837c2fc75f399407198f8479d9
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-14 20:26:40 -0400

    Creamos un servicio de autenticacion que verifica si el usuario esta logueado El usuario esta logueado cuando en el localstorage esta guardado el usuario

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  isLoggedIn(): boolean {
+    const user = localStorage.getItem('usuario');
+    return !!user;
+
+  }
+}
