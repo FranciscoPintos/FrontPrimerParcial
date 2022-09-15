80d9114050f5c5d23750b390d39bbdb425d54d3f
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-14 20:28:33 -0400

    Agregamos a las paginas los guards correspondientes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,25 @@ import { LoginComponent } from './features/auth/pages/login/login.component';
 import { CategoriaPageComponent } from './shared/pages/categoria-page/categoria-page.component';
 import { SubCategoriaPageComponent } from './shared/pages/sub-categoria-page/sub-categoria-page.component';
 import { CrearModificarServicioComponent } from './features/crear-modificar-servicio/crear-modificar-servicio.component';
-import {ListadoServicioComponent} from "./features/servicio/pages/listar-servicio/listado-servicio.component";
+import { ListadoServicioComponent } from "./features/servicio/pages/listar-servicio/listado-servicio.component";
 import { ReservasComponent } from "./features/reserva/pages/reservas/reservas.component";
 import { AuthGuard } from './shared/guards/auth.guard';
+import { HomeComponent } from './shared/pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     canActivate: [AuthGuard],
-    component: ListadoFichaClinicaPageComponent
+    component: HomeComponent,
   },
   {
     path: 'pais',
+    canActivate: [AuthGuard],
     component: PaisComponent
   },
   {
     path: 'nuevopais',
+    canActivate: [AuthGuard],
     component: PaisAgregarComponent
   },
   {
@@ -31,37 +34,42 @@ const routes: Routes = [
   },
   {
     path: 'reserva',
+    canActivate: [AuthGuard],
     component: ReservasComponent
   },
 
   {
     path: 'ficha_clinica',
+    canActivate: [AuthGuard],
     component: ListadoFichaClinicaPageComponent
   },
   {
     path: 'servicios',
+    canActivate: [AuthGuard],
     component: ListadoServicioComponent
   },
   {
     path: 'categorias',
+    canActivate: [AuthGuard],
     component: CategoriaPageComponent
   },
   {
     path: 'subcategorias',
+    canActivate: [AuthGuard],
     component: SubCategoriaPageComponent
   },
   {
     path: 'modservicio',
+    canActivate: [AuthGuard],
     component: CrearModificarServicioComponent
   },
   {
     path: 'listar_servicio',
+    canActivate: [AuthGuard],
     component: ListadoServicioComponent
   },
 
 ];
-
-
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
