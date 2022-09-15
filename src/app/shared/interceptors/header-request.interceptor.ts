19da2677b87ae6df88ee64a01147e14456de4eaa
Matias Sebastian Ramirez Brizuela <ramirezmatias946@gmail.com>
2022-09-15 14:20:56 -0400

    Agregamos el interceptor para agregar al header la peticion

---
+++ src/app/shared/interceptors/header-request.interceptor.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class HeaderRequestInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    if (this.authService.isLoggedIn()) {
+      const url = request.url;
+      const method = request.method;
+      if (method == "POST") {
+        let isValid = false;
+        if (url.includes("personaHorarioAgenda")) {
+          isValid = true;
+        }
+        else if (url.includes("horarioExcepcion")) {
+          isValid = true;
+        }
+        else if (url.includes("reserva")) {
+          isValid = true;
+        }
+        else if (url.includes("fichaClinica")) {
+          isValid = true;
+        }
+        else if (url.includes("servicio")) {
+          isValid = true;
+        }
+
+        if (isValid) {
+          const usuario = this.authService.getUsuario();
+          request = request.clone({
+            setHeaders: {
+              'usuario': usuario
+            }
+          });
+        }
+      }
+    }
+    console.log("request");
+    console.log(request);
+
+    return next.handle(request);
+  }
+}
